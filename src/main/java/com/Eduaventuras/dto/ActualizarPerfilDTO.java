cbb595ce6d281b280a07a962a202641eb0a578b7
Emanuel <emanuelgonzalezhio334@gmail.com>
2025-11-23 17:10:04 -0500

    feat: se implementa la actualizacion de perfil de usuario

---
+++ src/main/java/com/Eduaventuras/dto/ActualizarPerfilDTO.java
@@ -0,0 +1,17 @@
+package com.Eduaventuras.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ActualizarPerfilDTO {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+    private String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
+    @Size(min = 2, max = 100, message = "El apellido debe tener entre 2 y 100 caracteres")
+    private String apellido;
+}
